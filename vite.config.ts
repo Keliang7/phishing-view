b16a457c1878ce9a53725257a69ad7e676de5642
李昂旗 <1739484858@qq.com>
2024-03-19 20:12:41 +0800

    2012

--- vite.config.ts
+++ vite.config.ts
@@ -143,12 +143,12 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
           target: 'http://172.16.20.30:32080',
           // target: 'http://172.16.20.84:32080',
           changeOrigin: true
-        },
-        '/api/v2': {
-          target: 'http://172.16.20.30:32082',
-          // target: 'http://172.16.20.84:32080',
-          changeOrigin: true
         }
+        // '/api/v2': {
+        //   target: 'http://172.16.20.30:32082',
+        //   // target: 'http://172.16.20.84:32080',
+        //   changeOrigin: true
+        // }
       },
       hmr: {
         overlay: false
